30218420b4dcd5fcb386d3ffcb8723ddaf07fb4b
thinca <thinca@gmail.com>
2009-04-06 11:53:42 +0900

    Update an answer of the problem 012 with ruby.
    Use Integer#prime_division instead of factorize method.

--- 012/problem012.rb
+++ 012/problem012.rb
@@ -1,33 +1,18 @@
-def factorize(n)
-  factors = Hash.new(0)
-  i = 2
-  while i < n
-    if n % i == 0
-      factors[i] += 1
-      n /= i
-    else
-      i += 1
-    end
-  end
-  factors[n] += 1
-  return factors
-end
+require 'mathn'
 
 def factors_num(n)
-  factors = factorize(n)
-  return 2 if factors.empty?
-  return factors.values.map{|i| i + 1}.inject(:*)
+  return n.prime_division.map{|i| i[1] + 1}.inject(:*)
 end
 
 def problem012()
   i = 1
   trialgle_num = 1
   loop do
+    i += 1
+    trialgle_num += i
     if 500 < factors_num(trialgle_num)
       return trialgle_num
     end
-    i += 1
-    trialgle_num += i
   end
 end
 
